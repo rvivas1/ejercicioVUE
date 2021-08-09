dec38168ee3f2053d755e13d00cd147dc76c56b0
robeert005cls <robeert005@hotmail.com>
2021-08-08 22:54:29 -0500

    Agregue cambios en variables

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        name: "Rober",
-        phone: "311200",
-        age: "30"
-    }
+        name: "",
+        phone: "",
+        age: "",
+        rta1:"",
+        rta2: "",
+        rta3:""
+    },
+    methods: {
+        message() {
+            this.rta1= 'Nombre: ' + this.name;
+            this.rta2= 'Tel: ' + this.phone;
+            this.rta3= 'Edad: ' + this.age;
+        },
+    },
 });
