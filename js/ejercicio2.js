64cdc82d80eeaf56bc31d22c5e45c05402773953
robeert005cls <robeert005@hotmail.com>
2021-08-10 23:52:32 -0500

    ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app1',
+    data: {
+      desc: 0.2,
+      compra: "",
+      total: ""
+    },
+    methods: {
+        totalPagar() {
+            if (this.compra>= 1000000){
+                this.total=this.compra-(this.compra*this.desc);
+            }
+            else {
+                this.total= this.compra;
+            }
+        },
+    },
+  });
