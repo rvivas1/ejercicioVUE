14c3a18dce65876a751b9a49ecd93846ee4be367
robeert005cls <robeert005@hotmail.com>
2021-08-17 22:55:27 -0500

    Realicé ejercicio calEdad

--- js/Ejercicio5.js
+++ js/Ejercicio5.js
@@ -1,9 +1,23 @@
 var app = new Vue({
   el: '#app5',
   data: {
-
+    aNac: 0,
+    aAct: new Date().getFullYear(),
+    mayEdad: 18,
+    falEdad: 0,
+    edad: 0
   },
   methods: {
+    calcEdad() {
+      this.edad = this.aAct - this.aNac;
+      if (this.edad>=this.mayEdad){
+        alert('Ud es mayor de edad , su edad es: '+ this.edad);
+
+      }else{
+        this.falEdad = this.mayEdad-this.edad;
+        alert('Ud es menor de edad, le faltan '+ this.falEdad + 'año(s) para la mayoría de edad');
+      }
+    }
   }
 
 });
